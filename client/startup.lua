331a6f5f412b157c5b236b7bd66de33a07271044
ceselegend <ceselegend@users.noreply.github.com>
2015-01-17 21:36:38 +0100

    Change calling method
    Consider using user input, so that the script is actually launched at startup.

--- client/startup.lua
+++ client/startup.lua
@@ -1,7 +1,8 @@
 tArgs={...}
-if type(tArgs[1]) ~= "string" then
-	error("Use SMS <pseudo>")
-end
+local pseudo = nil
+print("Please enter your pseudo")
+pseudo = read()
+
 periph = peripheral.getNames()
 side = nil
 for u,v in pairs(periph) do
@@ -9,10 +10,12 @@ for u,v in pairs(periph) do
 		side = u
 	end
 end
+
 if side == nil then
 	error("No modem is connected to the computer")
 elseif not peripheral.call(side,"isWireless") then
 	error("The modem is not wireless")
 end
+
 rednet.open(side)
-rednet.host("SMS",tArgs[1])
+rednet.host("SMS",pseudo)
