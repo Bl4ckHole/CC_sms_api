28b8ae604cb6440414e14d775e3216967ceca815
ceselegend <ceselegend@users.noreply.github.com>
2015-01-17 22:04:56 +0100

    Simplify peripheral getting
    Wireless pocket computers : modem are always at the back.

--- client/startup.lua
+++ client/startup.lua
@@ -3,19 +3,21 @@ local pseudo = nil
 print("Please enter your pseudo")
 pseudo = read()
 
-periph = peripheral.getNames()
-side = nil
-for u,v in pairs(periph) do
-	if peripheral.getType(u) == "modem" then
-		side = u
-	end
-end
+--periph = peripheral.getNames()
+--side = nil
+--for u,v in pairs(periph) do
+--	if peripheral.getType(u) == "modem" then
+--		side = u
+--	end
+--end
 
-if side == nil then
-	error("No modem is connected to the computer")
-elseif not peripheral.call(side,"isWireless") then
-	error("The modem is not wireless")
-end
+--if side == nil then
+--	error("No modem is connected to the computer")
+--elseif not peripheral.call(side,"isWireless") then
+--	error("The modem is not wireless")
+--end
+
+local modem = peripheral.wrap("back")
 
 rednet.open(side)
 rednet.host("SMS",pseudo)
