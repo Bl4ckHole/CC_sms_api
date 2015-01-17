73992525be19ad8bd67fde264b1eca6346fcca07
ceselegend <ceselegend@users.noreply.github.com>
2015-01-17 22:32:42 +0100

    Removed comments

--- client/startup.lua
+++ client/startup.lua
@@ -3,20 +3,6 @@ local pseudo = nil
 print("Please enter your pseudo")
 pseudo = read()
 
---periph = peripheral.getNames()
---side = nil
---for u,v in pairs(periph) do
---	if peripheral.getType(u) == "modem" then
---		side = u
---	end
---end
-
---if side == nil then
---	error("No modem is connected to the computer")
---elseif not peripheral.call(side,"isWireless") then
---	error("The modem is not wireless")
---end
-
 function sendSMS(target, message)
 	local ID = rednet.lookup("SMS", target)
 	rednet.send(ID, message, "SMS")
