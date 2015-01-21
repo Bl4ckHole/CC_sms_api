1e8c10580ed00179fc3c9344ed34198ed9a56f50
ceselegend <ceselegend@users.noreply.github.com>
2015-01-21 02:02:11 +0100

    Deported all function to API.

--- client/startup.lua
+++ client/startup.lua
@@ -3,41 +3,12 @@ local pseudo = nil
 baseShellID = multishell.getFocus()
 print("Please enter your pseudo")
 pseudo = read()
+--Add pseudo to message so that we can easily check who's the sender
+os.loadAPI("smsAPI")
 
-function sendSMS(recipient, message)
-	local ackMessage, receiver = nil
-	local recipientID = rednet.lookup("SMS", recipient)
-	rednet.send(recipientID, message, "SMS")
-	-- Waiting for ack
-	listenerTabID = multishell.launch({ackMessage, receiver}, "/listener", "ack")
-	multishell.setTitle(listenerTabID, "Listener tab")
-	multishell.setFocus(baseShellID) 
-	if receiver == recipientID and ackMessage == "#!ACK!#" then 
-		print("Message received by recipient")
-	else
-		print("Failed to send message.\nEither you or the recipient are too far from any relay")
-	end
-end
-
-function listener(mode)
-	local timeout = nil
-	if mode == "ack" then
-		timeout = 5
-	end
-	local sender, message, protocol = rednet.receive("SMS", timeout)
-	return sender, message
-end
-
-function displaySMS(sender,message)
-	print("You've got a message from "..sender.." :\n")
-	print(message)
-end
-
-local modem = peripheral.wrap("back")
-
-rednet.open("back")
+local listenerTabID = multishell.launch({}, "listener")
+local modem = peripheral.wrap("left")
+rednet.open("left")
 rednet.host("SMS",pseudo)
 
-
-
-
+smsAPI.sendSMS("ceselegend","Bonjour")
