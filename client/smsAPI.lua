d1fa0e9a3a3dba1fec31d7bd9394bc591e1114ab
ceselegend <ceselegend@users.noreply.github.com>
2015-01-21 02:01:01 +0100

    Moved all function to API

--- client/smsAPI.lua
+++ client/smsAPI.lua
@@ -1,43 +1,38 @@
-tArgs={...}
-local pseudo = nil
-baseShellID = multishell.getFocus()
-print("Please enter your pseudo")
-pseudo = read()
+local ack = "#!ACK!#"
 
 function sendSMS(recipient, message)
 	local ackMessage, receiver = nil
 	local recipientID = rednet.lookup("SMS", recipient)
-	rednet.send(recipientID, message, "SMS")
-	-- Waiting for ack
-	listenerTabID = multishell.launch({ackMessage, receiver}, "/listener", "ack")
-	multishell.setTitle(listenerTabID, "Listener tab")
-	multishell.setFocus(baseShellID) 
-	if receiver == recipientID and ackMessage == "#!ACK!#" then 
-		print("Message received by recipient")
+	if recipientID == nil then
+		print("Recipient not found. Either your recipient has is phone off or you mispell his pseudo.\n")
 	else
-		print("Failed to send message.\nEither you or the recipient are too far from any relay")
+		rednet.send(recipientID, message, "SMS")
+		receiver, ackMessage = listener("ack")
+		if receiver == recipientID and ackMessage == ack then
+			print("Message receive by recipient")
+		else
+			print("Failed to send message.\nEither you or the recipient are too far from any relay\n")
+		end
 	end
 end
 
 function listener(mode)
 	local timeout = nil
 	if mode == "ack" then
-		timeout = 5
+		timeout = 1
 	end
 	local sender, message, protocol = rednet.receive("SMS", timeout)
 	return sender, message
 end
 
-function displaySMS(sender,message)
+function displaySMS(sender, message)
 	print("You've got a message from "..sender.." :\n")
-	print(message)
+	print(message.."\n")
+	local log = fs.open("log", "a")
+	log.write(sender.." : "..message.."\n")
+	log.close()
 end
 
-local modem = peripheral.wrap("back")
-
-rednet.open("back")
-rednet.host("SMS",pseudo)
-
-
-
-
+function sendAck(sender)
+	rednet.send(sender, ack, "SMS")
+end
